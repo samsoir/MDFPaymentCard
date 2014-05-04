012047c768d8351adde57d7da9d8d71d16a5cccb
Sam de Freyssinet <sam@def.reyssi.net>
2014-05-03 21:16:07 -0500

    Initial Commit

---
+++ MDFPaymentCard/MDFPaymentCard.h
@@ -0,0 +1,13 @@
+//
+//  MDFPaymentCard.h
+//  MDFPaymentCard
+//
+//  Created by Sam de Freyssinet on 03/05/2014.
+//  Copyright (c) 2014 Maison de Freyssinet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MDFPaymentCard : NSObject
+
+@end
---
+++ MDFPaymentCard/MDFPaymentCard.m
@@ -0,0 +1,13 @@
+//
+//  MDFPaymentCard.m
+//  MDFPaymentCard
+//
+//  Created by Sam de Freyssinet on 03/05/2014.
+//  Copyright (c) 2014 Maison de Freyssinet. All rights reserved.
+//
+
+#import "MDFPaymentCard.h"
+
+@implementation MDFPaymentCard
+
+@end
